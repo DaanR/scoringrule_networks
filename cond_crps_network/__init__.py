7f12f2bc41723a94e46038331eecc28d8b68668e
DaanR <daan.roordink@solarteameindhoven.nl>
2023-01-08 21:06:03 +0100

    Hello world commit, basic folder structure

---
+++ cond_crps_network/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 21:01:57 2023
+
+@author: daan
+"""
+
---
+++ usage_example.py
@@ -0,0 +1,7 @@
+import quantile_network
+
+
+
+
+if __name__ == "__main__":
+    print("Hello world!")
