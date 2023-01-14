4c7b1ab52eaea6790ed7c377bd266a99a8d76e51
DaanR <daan.roordink@solarteameindhoven.nl>
2023-01-14 17:58:33 +0100

    Changed setup.py, no idea whether this works, yet to test

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup, find_packages
+ from setuptools import setup, find_packages
 
-setup(name='myproject', version='1.0', packages=find_packages())
+
+setup(name='scoringrule_networks',
+      version='1.0.0',
+      description='Scoring rule implementations for Tensorflow',
+      long_description=readme(),
+      url='https://github.com/DaanR/scoringrule_networks',
+      author='Daan Roordink',
+      author_email='daan.roordink@gmail.com',
+      install_requires=[
+          'pandas',
+          'rpy2',
+          'numpy',
+          'tensorflow',
+          'keras',
+          'tensorflow_probability'
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+      ],
+      include_package_data=False, #For now
+      packages=find_packages(),
